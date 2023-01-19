c93673a083b8ac113c8ffb79eb7e34de58c602aa
aleksey-saenko <aleksei.saenko.gm@gmail.com>
2023-01-20 00:53:39 +0300

    init commit

---
+++ app/src/main/java/com/mrsep/musicrecognizer/MainActivity.kt
@@ -0,0 +1,44 @@
+package com.mrsep.musicrecognizer
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mrsep.musicrecognizer.ui.theme.MusicRecognizerTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            MusicRecognizerTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    MusicRecognizerTheme {
+        Greeting("Android")
+    }
+}

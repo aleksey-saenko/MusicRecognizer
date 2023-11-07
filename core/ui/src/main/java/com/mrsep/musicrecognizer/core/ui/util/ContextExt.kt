e918b5564669378e054d4ea35191b5bac289e9ee
aleksey-saenko <aleksei.saenko.gm@gmail.com>
2023-11-07 18:26:14 +0300

    Fixed music services app link opening.

--- core/ui/src/main/java/com/mrsep/musicrecognizer/core/ui/util/ContextExt.kt
+++ core/ui/src/main/java/com/mrsep/musicrecognizer/core/ui/util/ContextExt.kt
@@ -1,17 +1,17 @@
 package com.mrsep.musicrecognizer.core.ui.util
 
+import android.content.ActivityNotFoundException
 import android.content.Context
 import android.content.Intent
 import android.net.Uri
 import android.widget.Toast
 import com.mrsep.musicrecognizer.core.strings.R as StringsR
 
-// FIXME: Youtube links are broken if youtube app is not installed
-// https://issuetracker.google.com/issues/243678703
-/** Validate URL before try to open  */
 fun Context.openUrlImplicitly(url: String) {
-    val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-    startActivityOrToast(intent, getString(StringsR.string.web_browser_not_found_toast))
+    startActivityOrToast(
+        Intent(Intent.ACTION_VIEW, Uri.parse(url)),
+        getString(StringsR.string.web_browser_not_found_toast)
+    )
 }
 
 fun Context.shareImageWithText(subject: String, body: String, imageUri: Uri) {
@@ -41,9 +41,9 @@ fun Context.shareText(subject: String, body: String) {
 }
 
 private fun Context.startActivityOrToast(intent: Intent, message: String) {
-    if (intent.resolveActivity(packageManager) != null) {
+    try {
         startActivity(intent)
-    } else {
+    } catch (e: ActivityNotFoundException) {
         Toast.makeText(this, message, Toast.LENGTH_LONG).show()
     }
 }

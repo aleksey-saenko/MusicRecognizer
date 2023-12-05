f8d1b6ab94275b72ae365f3078e98b10b0e1aa0e
aleksey-saenko <aleksei.saenko.gm@gmail.com>
2023-12-05 12:17:30 +0300

    Migrate preferences serializer to Kotlin DSL

--- data/src/main/java/com/mrsep/musicrecognizer/data/preferences/UserPreferencesProtoSerializer.kt
+++ data/src/main/java/com/mrsep/musicrecognizer/data/preferences/UserPreferencesProtoSerializer.kt
@@ -5,65 +5,72 @@ import androidx.datastore.core.Serializer
 import com.google.protobuf.InvalidProtocolBufferException
 import com.mrsep.musicrecognizer.MusicServiceProto
 import com.mrsep.musicrecognizer.UserPreferencesProto
+import com.mrsep.musicrecognizer.UserPreferencesProtoKt.fallbackPolicyProto
+import com.mrsep.musicrecognizer.UserPreferencesProtoKt.hapticFeedbackProto
+import com.mrsep.musicrecognizer.UserPreferencesProtoKt.lyricsFontStyleProto
+import com.mrsep.musicrecognizer.UserPreferencesProtoKt.trackFilterProto
 import com.mrsep.musicrecognizer.data.BuildConfig
+import com.mrsep.musicrecognizer.userPreferencesProto
 import java.io.InputStream
 import java.io.OutputStream
 
 object UserPreferencesProtoSerializer : Serializer<UserPreferencesProto> {
 
     override val defaultValue: UserPreferencesProto
-        get() = UserPreferencesProto.newBuilder()
-            .setOnboardingCompleted(false)
-            .setApiToken(BuildConfig.AUDD_TOKEN)
-            .addAllRequiredMusicServices(
-                // add default audd services
+        get() = userPreferencesProto {
+            onboardingCompleted = false
+            apiToken = BuildConfig.AUDD_TOKEN
+            requiredMusicServices.addAll(
+                // ordered by popularity
                 listOf(
                     MusicServiceProto.Spotify,
-                    MusicServiceProto.Youtube,
-                    MusicServiceProto.Soundcloud,
                     MusicServiceProto.AppleMusic,
+                    MusicServiceProto.Youtube,
+                    MusicServiceProto.YoutubeMusic,
+                    MusicServiceProto.AmazonMusic,
                     MusicServiceProto.Deezer,
-                    MusicServiceProto.MusicBrainz,
+                    MusicServiceProto.Tidal,
+                    MusicServiceProto.Soundcloud,
+                    MusicServiceProto.YandexMusic,
                     MusicServiceProto.Napster,
+                    MusicServiceProto.Pandora,
+                    MusicServiceProto.Anghami,
+                    MusicServiceProto.Audiomack,
+                    MusicServiceProto.Audius,
+                    MusicServiceProto.Boomplay,
+                    MusicServiceProto.MusicBrainz,
                 )
             )
-            .setDynamicColorsEnabled(true)
-            .setArtworkBasedThemeEnabled(false)
-            .setUseColumnForLibrary(false)
-            .setNotificationServiceEnabled(false)
-            .setDeveloperModeEnabled(true)
-            .setFallbackPolicy(
-                UserPreferencesProto.FallbackPolicyProto.newBuilder()
-                    .setNoMatches(UserPreferencesProto.FallbackActionProto.IGNORE)
-                    .setBadConnection(UserPreferencesProto.FallbackActionProto.SAVE_AND_LAUNCH)
-                    .setAnotherFailure(UserPreferencesProto.FallbackActionProto.SAVE)
-                    .build()
-            )
-            .setLyricsFontStyle(
-                UserPreferencesProto.LyricsFontStyleProto.newBuilder()
-                    .setFontSize(UserPreferencesProto.FontSizeProto.NORMAL)
-                    .setIsBold(false)
-                    .setIsHighContrast(false)
-            )
-            .setTrackFilter(
-                UserPreferencesProto.TrackFilterProto.newBuilder()
-                    .setFavoritesMode(UserPreferencesProto.FavoritesModeProto.ALL)
-                    .setSortBy(UserPreferencesProto.SortByProto.RECOGNITION_DATE)
-                    .setOrderBy(UserPreferencesProto.OrderByProto.DESC)
-                    .setStartDate(Long.MIN_VALUE)
-                    .setEndDate(Long.MAX_VALUE)
-                    .build()
-            )
-            .setHapticFeedback(
-                UserPreferencesProto.HapticFeedbackProto.newBuilder()
-                    .setVibrateOnTap(false)
-                    .setVibrateOnResult(false)
-                    .build()
-            )
-            .setThemeMode(UserPreferencesProto.ThemeModeProto.FOLLOW_SYSTEM)
-            .setUsePureBlackForDarkTheme(false)
-            .setHasDoneRequiredMusicServicesMigration(true)
-            .build()
+            notificationServiceEnabled = false
+            dynamicColorsEnabled = true
+            artworkBasedThemeEnabled = false
+            useColumnForLibrary = false
+            developerModeEnabled = false // was true in first public builds, reset for using
+            fallbackPolicy = fallbackPolicyProto {
+                noMatches = UserPreferencesProto.FallbackActionProto.IGNORE
+                badConnection = UserPreferencesProto.FallbackActionProto.SAVE_AND_LAUNCH
+                anotherFailure = UserPreferencesProto.FallbackActionProto.SAVE
+            }
+            lyricsFontStyle = lyricsFontStyleProto {
+                fontSize = UserPreferencesProto.FontSizeProto.NORMAL
+                isBold = false
+                isHighContrast = false
+            }
+            trackFilter = trackFilterProto {
+                favoritesMode = UserPreferencesProto.FavoritesModeProto.ALL
+                sortBy = UserPreferencesProto.SortByProto.RECOGNITION_DATE
+                orderBy = UserPreferencesProto.OrderByProto.DESC
+                startDate = Long.MIN_VALUE
+                endDate = Long.MAX_VALUE
+            }
+            hapticFeedback = hapticFeedbackProto {
+                vibrateOnTap = true
+                vibrateOnResult = true
+            }
+            themeMode = UserPreferencesProto.ThemeModeProto.FOLLOW_SYSTEM
+            usePureBlackForDarkTheme = false
+            hasDoneRequiredMusicServicesMigration = true
+        }
 
     override suspend fun readFrom(input: InputStream): UserPreferencesProto {
         try {

732d02c81f33462951f8bc5f15d1f6f9816f383e
aleksey-saenko <aleksei.saenko.gm@gmail.com>
2024-10-23 20:22:09 +0300

    Refactor ConnectivityManagerNetworkMonitor

--- data/src/main/java/com/mrsep/musicrecognizer/data/ConnectivityManagerNetworkMonitor.kt
+++ data/src/main/java/com/mrsep/musicrecognizer/data/ConnectivityManagerNetworkMonitor.kt
@@ -2,57 +2,55 @@ package com.mrsep.musicrecognizer.data
 
 import android.content.Context
 import android.net.ConnectivityManager
+import android.net.ConnectivityManager.NetworkCallback
 import android.net.Network
 import android.net.NetworkCapabilities
-import android.net.NetworkRequest
+import android.net.NetworkRequest.Builder
 import androidx.core.content.getSystemService
+import com.mrsep.musicrecognizer.core.common.di.IoDispatcher
 import dagger.hilt.android.qualifiers.ApplicationContext
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.channels.awaitClose
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.callbackFlow
 import kotlinx.coroutines.flow.conflate
 import kotlinx.coroutines.flow.distinctUntilChanged
+import kotlinx.coroutines.flow.flowOn
 import javax.inject.Inject
 
 internal class ConnectivityManagerNetworkMonitor @Inject constructor(
-    @ApplicationContext private val context: Context
+    @ApplicationContext private val context: Context,
+    @IoDispatcher private val ioDispatcher: CoroutineDispatcher,
 ) : NetworkMonitorDo {
+
     override val isOffline: Flow<Boolean> = callbackFlow {
         val connectivityManager = context.getSystemService<ConnectivityManager>()
         if (connectivityManager == null) {
-            channel.trySend(false)
+            channel.trySend(true)
             channel.close()
             return@callbackFlow
         }
 
-        fun update() {
-            channel.trySend(connectivityManager.isCurrentlyConnected().not())
+        val callback = object : NetworkCallback() {
+            private val networks = mutableSetOf<Network>()
+
+            override fun onAvailable(network: Network) {
+                networks += network
+                channel.trySend(false)
+            }
+
+            override fun onLost(network: Network) {
+                networks -= network
+                channel.trySend(networks.isEmpty())
+            }
         }
 
-        /**
-         * The callback's methods are invoked on changes to *any* network, not just the active
-         * network. So to check for network connectivity, one must query the active network of the
-         * ConnectivityManager.
-         */
-        val callback = object : ConnectivityManager.NetworkCallback() {
-            override fun onAvailable(network: Network) = update()
+        val request = Builder()
+            .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
+            .build()
+        connectivityManager.registerNetworkCallback(request, callback)
 
-            override fun onLost(network: Network) = update()
-
-            override fun onCapabilitiesChanged(
-                network: Network,
-                networkCapabilities: NetworkCapabilities,
-            ) = update()
-        }
-
-        connectivityManager.registerNetworkCallback(
-            NetworkRequest.Builder()
-                .addCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET)
-                .build(),
-            callback,
-        )
-
-        update()
+        channel.trySend(connectivityManager.isCurrentlyConnected().not())
 
         awaitClose {
             connectivityManager.unregisterNetworkCallback(callback)
@@ -60,9 +58,11 @@ internal class ConnectivityManagerNetworkMonitor @Inject constructor(
     }
         .distinctUntilChanged()
         .conflate()
+        .flowOn(ioDispatcher)
 
-    private fun ConnectivityManager.isCurrentlyConnected() =
-        activeNetwork
+    private fun ConnectivityManager.isCurrentlyConnected(): Boolean {
+        return activeNetwork
             ?.let(::getNetworkCapabilities)
             ?.hasCapability(NetworkCapabilities.NET_CAPABILITY_INTERNET) ?: false
+    }
 }

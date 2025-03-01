6a14e18f6b9e72ae0e91a5ee56f89903554b5b68
aleksey-saenko <aleksei.saenko.gm@gmail.com>
2025-03-02 02:34:24 +0300

    Update metadata

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
 
     defaultConfig {
         applicationId = "com.mrsep.musicrecognizer"
-        versionCode = 27
-        versionName = "1.7.3"
+        versionCode = 28
+        versionName = "1.8.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
